29d4db02f0b05cdbb0e50996dd9d6917c893ad11
Sindarius <pyroclasmx@yahoo.com>
2020-11-02 06:08:32 -0600

    Updated plugin index.js to reflect changes in DWC for ui injection

--- GCodeViewer/index.js
+++ GCodeViewer/index.js
@@ -1,18 +1,18 @@
-import { registerRoute } from '../../routes'
-import { registerPluginContextMenuItem, ContextMenuType } from '../../store/uiinjection.js'
-import GCodeViewer from './GCodeViewer.vue'
-import ColorPicker from './ColorPicker.vue'
-import Vue from 'vue'
+import { registerRoute } from '../../routes';
+import { registerPluginContextMenuItem, ContextMenuType } from '../';
+import GCodeViewer from './GCodeViewer.vue';
+import ColorPicker from './ColorPicker.vue';
+import Vue from 'vue';
 Vue.component('gcodeviewer-color-picker', ColorPicker);
 
 registerRoute(GCodeViewer, {
-    Job: {
-        GCodeViewer: {
-            icon: 'mdi-rotate-3d',
-            caption: 'GCode Viewer',
-            path: '/GCodeViewer'
-        }
-    }
+  Job: {
+    GCodeViewer: {
+      icon: 'mdi-rotate-3d',
+      caption: 'GCode Viewer',
+      path: '/GCodeViewer',
+    },
+  },
 });
 
 registerPluginContextMenuItem('View 3D', '/GCodeViewer', 'mdi-rotate-3d', 'view-3d-model', ContextMenuType.JobFileList);
